514ebf87a6812677d206847e3aeb6013ae38cde1
xinrou <22014057@myrp.edu.sg>
2024-12-23 16:54:02 +0800

    working navbar php

---
+++ navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg p-3">
+    <div class="container-fluid">
+        <div class="navbar-brand">
+            <div style="display: flex; align-items: center;">
+                <div style="width: 50px; height: 50px; background-color: #102042; border-radius: 50%;"></div>
+            </div>
+        </div>
+        <div>
+            <a class="btn me-2" style="background-color: #E8C766;" href="MyPoint.html">My Points</a>
+            <a class="btn me-2" style="background-color: #E8C766;" href="#">Events</a>
+            <a class="btn " style="background-color: #E8C766;" href="#">My Profile</a>
+        </div>
+    </div>
+</nav>
