ecf9377eb76ff5753003069693d76b9541232f27
hidayah7021 <22007021@myrp.edu.sg>
2025-01-09 14:21:07 +0800

    email is in the form

--- add_user.php
+++ add_user.php
@@ -152,6 +152,9 @@ $result = $conn->query($sql);
         <form method="POST">
             <label for="username">Username:</label>
             <input type="text" id="username" name="Username" required>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="Email" required>
             
             <label for="password">Password:</label>
             <input type="password" id="password" name="Password" required>
